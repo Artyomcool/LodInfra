6eb8697ff2d0022c65f92faabe0d0f58f221bd94
Artyom Drozdov <artyomcool2@gmail.com>
2024-10-10 18:47:16 +0200

    Version bump

--- Infra/src/main/java/com/github/artyomcool/lodinfra/Pack.java
+++ Infra/src/main/java/com/github/artyomcool/lodinfra/Pack.java
@@ -19,7 +19,7 @@ import java.util.zip.DataFormatException;
 
 public class Pack {
 
-    private static final String VERSION = "1.5";
+    private static final String VERSION = "1.6";
     public static Application APP = null;
 
     public static void main(String[] a) throws IOException {
